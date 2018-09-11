397d451bc73f7e4b54262f759b021be6a15c0eaa
Gabriel Gasparini Iadocicco <gabrielgasparini11@gmail.com>
2018-09-11 17:45:26 -0300

    Autênticação adiciona a área administrativa

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,14 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">    
 
     <title>Sistema de gestão de portfólio financeiro</title>
 
@@ -15,19 +18,27 @@
     <!-- Custom styles for this template -->
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
     <link href="{{ asset('css/blog.css') }}" rel="stylesheet">
-  </head>
 
-  <body>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
 
     @include ('layouts.nav')
     
     <main role="main" class="container">
-      
+
         @yield ('content')
 
     </main>
 
     @include ('layouts.footer')
     
-  </body>
+</body>
 </html>
